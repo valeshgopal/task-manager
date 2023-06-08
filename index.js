d287325706f9f7e56a7de990414a4838cd7d16fc
Valesh Gopal <valesh.dev@gmail.com>
2023-06-08 21:56:18 +0530

    add port in env

--- index.js
+++ index.js
@@ -25,13 +25,14 @@ app.get('*', function (_, res) {
 });
 
 const MONGO_URI = process.env.MONGO_URI;
+const PORT = process.env.PORT || 5000;
 
 const connectToDB = () =>
   mongoose
     .connect(MONGO_URI)
     .then(() => {
-      app.listen(5000, () => {
-        console.log('Server started listening on port 5000...');
+      app.listen(PORT, () => {
+        console.log(`Server started listening on port ${PORT}...`);
       });
     })
     .catch((err) => console.log(err));
